2dc762a60ad2cfcbbeb2b27bec6ae4aa86740559
Yahia El Tai <yahiaeltai022@gmail.com>
2022-10-23 12:53:06 +0400

    initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const { json } = require("body-parser");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(json());
+app.use(cors());
+
+const generateRandomInteger = () => Math.floor(Math.random() * 10) + 1;
+
+const prices = [
+  "10.00",
+  "12.45",
+  "15.30",
+  "20.50",
+  "12.50",
+  "32.00",
+  "7.00",
+  "40.00",
+  "25.00",
+  "29.00",
+];
+
+const API_URL = "https://www.themealdb.com/api/json/v1/1/filter.php?c=";
+
+app.get("/api/meals", async (req, res) => {
+  const breakfast = await axios.get(`${API_URL}Breakfast`);
+  const dessert = await axios.get(`${API_URL}Dessert`);
+  const vegetarian = await axios.get(`${API_URL}Vegetarian`);
+  const vegan = await axios.get(`${API_URL}Vegan`);
+
+  const allMeals = [
+    ...breakfast.data.meals,
+    ...vegetarian.data.meals,
+    ...vegan.data.meals,
+  ];
+
+  const mealsWithPrices = allMeals.map((meal) => ({
+    ...meal,
+    price: prices[generateRandomInteger() - 1],
+  }));
+
+  res.send(mealsWithPrices);
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000!");
+});
